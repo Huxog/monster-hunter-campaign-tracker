e4d395ec4ca08243aaeffb70d7eaaa370839465c
Mauro Diosdado <mauro_diosdado@hotmail.com>
2025-07-10 01:24:01 -0500

    fix: Correct route definition for hunters in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CampaignController;
+use App\Http\Controllers\HunterController;
 use App\Http\Controllers\MapController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('maps', MapController::class);
 Route::apiResource('campaings', CampaignController::class);
+Route::apiResource('hunters', HunterController::class);
